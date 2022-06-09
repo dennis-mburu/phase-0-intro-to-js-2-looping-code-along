7064ed577f775a5f55f729aae1e5f67aa57db0c0
Dennnis Mburu Mwaura <dennismburu2063@gmail.com>
2022-06-09 15:47:34 +0300

    Assignment Complete

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+const event = "surprise";
+let newArray = [];
+let message;
+
+function writeCards(names, event){
+    for(let i=0; i<names.length; i++){
+        message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+        newArray.push(message);
+    }
+    return newArray;
+}
+
+function countDown(){
+    let num = 0
+    while (num >=0 && num <=10){
+        console.log(num);
+        num++
+    }
+}
